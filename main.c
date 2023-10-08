0102365e88d5ef8489a535e020a9976b4bd86aaf
qksekf28 <hayoung902847@gmail.com>
2023-10-08 23:51:07 +0900

    05

--- main.c
+++ main.c
@@ -5,17 +5,21 @@
 
 int main(int argc, char *argv[]) {
 	
-   int year;
+   unsigned int x;
+   int b;
    
-   printf("input a year : ");
-   scanf("%d", &year);
+   printf("input a number : ");
+   scanf("%ui", &x); // %ui=unsignt integer
    
-   if ( (year%4==0 && year%100!=0) || year%400==0 )
-   { printf("%i is leap year.\n", year);
+   for (b=0; x != 0; x >>= 1) // -> x = x >> 1
+   {
+   	if (x & 1)
+   	{
+   		b++;
+	   }
    }
-   else
-   { printf("%i is not leap year.\n", year);
-   }
+   
+   printf("The result is : %i\n", b);
 
 	return 0;
 }
